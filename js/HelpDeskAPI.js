4a23f71b13330507de292697e84cebbd4e15cb90
Eugene <jtrue@mail.ru>
2012-06-22 15:15:46 +0300

    added localStorage logic

--- js/HelpDeskAPI.js
+++ js/HelpDeskAPI.js
@@ -12,21 +12,20 @@
  * @return Instance of {@link HelpDeskAPI}
  */
 
-var HelpDeskAPI = function (login, pass, options) {
+var HelpDeskAPI = function (options) {
 	
     if (!options)
         var options = {};
 
-    if (!login || !pass)
-        throw 'You have to provide an login and pass for this to work.';
+    //if (!login || !pass)
+    //    throw 'You have to provide an login and pass for this to work.';
 
 	this.version     = '1.0';
-	this.login      = login;
-    this.pass      = pass;
+	this.login      = localStorage.getItem('login');
+    this.pass      = localStorage.getItem('password');
 	this.secure      = options.secure || false;
 	this.packageInfo = options.packageInfo;
-    this.httpHost    = login + ':' + pass + '@' + 'api.beta.helpdesk.bigwebapps.com';
-	this.httpUri     = (this.secure) ? 'https://'+this.httpHost+':443' : 'http://'+this.httpHost+':80';	
+    this.httpHost    = 'api.beta.helpdesk.bigwebapps.com';
 }
 
 /**
@@ -42,35 +41,38 @@ var HelpDeskAPI = function (login, pass, options) {
  */
 HelpDeskAPI.prototype.execute = function (method, availableParams, givenParams, callback) {
 
-	var finalParams = { login : this.login, password: this.pass };
-	
-	for (var i = 0; i < availableParams.length; i++) {
-		currentParam = availableParams[i];
-		if (typeof givenParams[currentParam] !== 'undefined')
-			finalParams[currentParam] = givenParams[currentParam];
-	}
+    var finalParams = {};// {login:this.login, password:this.pass };
 
+    for (var i = 0; i < availableParams.length; i++) {
+        currentParam = availableParams[i];
+        if (typeof givenParams[currentParam] !== 'undefined')
+            finalParams[currentParam] = givenParams[currentParam];
+    }
+
+    var basicUrl = this.login + ':' + this.pass + '@' + this.httpHost;
+    this.httpUri     = (this.secure) ? 'https://'+basicUrl+':443' : 'http://'+basicUrl+':80';
+
+    console.log(this.httpUri + '/' + method + '?callback=?');
     $.ajax({
-        url: this.httpUri+'/'+method+'?callback=?',
-        type: 'GET',
-        cache: false,
-        dataType: "json",
-        data: finalParams,
-        contentType: "application/json; charset=utf-8",
-        timeout: 5000,
-        success: function(data) {
+        url: this.httpUri + '/' + method + '?callback=?',
+        type:'GET',
+        cache:false,
+        dataType:"json",
+        data:finalParams,
+        contentType:"application/json; charset=utf-8",
+        timeout:5000,
+        success:function (data) {
             callback(data);
         },
-        error: function(jqXHR, textStatus, errorThrown) {
-            if (errorThrown == 'timeout')
-            {
+        error:function (jqXHR, textStatus, errorThrown) {
+            if (errorThrown == 'timeout') {
                 console.log('401');
                 window.location.replace('login.html');
             }
             else if (errorThrown == 'parsererror')
-                callback({ 'error' : 'Error parsing JSON answer from  HelpDeskAPI.', 'code' : 'xxx' });
+                callback({ 'error':'Error parsing JSON answer from  HelpDeskAPI.', 'code':'xxx' });
             else
-                callback({ 'error' : 'Unable to connect to the  HelpDeskAPI endpoint.', 'code' : 'xxx' });
+                callback({ 'error':'Unable to connect to the  HelpDeskAPI endpoint.', 'code':'xxx' });
         }
     });
 }
@@ -80,11 +82,30 @@ HelpDeskAPI.prototype.execute = function (method, availableParams, givenParams, 
 /*****************************************************************************/
 
 /**
+ * Login user.
+ *
+ * @see http://developer.helpdesk.bigwebapps.com/
+ */
+HelpDeskAPI.prototype.loginUser = function (params, callback) {
+    if (typeof params == 'function') callback = params, params = {};
+    else
+    {
+        this.login      = params[0];
+        this.pass      = params[1];
+        localStorage.setItem('login', this.login);
+        localStorage.setItem('password', this.pass);
+    }
+    this.execute('login/organizations', ["login", "pass"
+    ], params, callback);
+}
+
+/**
  * Get list of user organizations and instances.
  *
  * @see http://developer.helpdesk.bigwebapps.com/
  */
-HelpDeskAPI.prototype.organizations = function (params, callback) {
+HelpDeskAPI.prototype.organizations = function (callback) {
+    var params = [];
     if (typeof params == 'function') callback = params, params = {};
     this.execute('login/organizations', [
     ], params, callback);
