da6f1975dd5fe7c62477ef5b4d5ba1717ffa4099
evgeniynet <jtrue@mail.ru>
2012-08-15 12:02:51 +0300

    updated connection check

--- js/helpdesk.js
+++ js/helpdesk.js
@@ -5,6 +5,29 @@
 
 api = new HelpDeskAPI();
 
+//region Connection
+function checkConnection() {
+    var networkState = navigator.network.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    if(states[networkState]==states[Connection.NONE] || states[networkState]==states[Connection.UNKNOWN]){
+        alert('A network connection is required to access this page.');
+        return false;
+    } else {
+        alert(states[networkState]);
+        return true;
+    }
+}
+//endregion
+
 //region Storage Functions
 function getStorage(key)
 {
