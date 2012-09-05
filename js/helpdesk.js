ceaf958484d737d5dc218d78e74827dd44fe5303
benpfohl <benpfohl@truenorthwebdesign.com>
2012-09-05 10:37:33 -0400

    small css changes to pop

--- js/helpdesk.js
+++ js/helpdesk.js
@@ -677,10 +677,10 @@ Handlebars.getTemplate = function(name) {
 
 function tooltip(message, type)
 {
-     $("<div class='ui-popup ui-overlay-shadow ui-corner-all ui-body-c ui-content ui-loader " + type + " ' data-theme='c'><p>"+message+"</p></div>").css({ "width" : "85%", "padding": "5px 5px", "display": "block", "opacity": 0.96, "left": "3.5%", "margin": ".5em 0","text-align": "center"})
+     $("<div class='ui-popup ui-overlay-shadow ui-corner-all ui-body-c ui-content ui-loader " + type + " ' data-theme='c'><p><strong>"+message+"</strong></p></div>").css({ "width" : "85%", "padding": "5px 5px", "display": "block", "opacity": 0.96, "left": "3.3%", "margin": ".5em 0","text-align": "center"})
      .appendTo( $.mobile.pageContainer )
      .delay( 1500 )
-     .fadeOut(500, function(){
+     .fadeOut(600, function(){
      $(this).remove();
      }
      );
